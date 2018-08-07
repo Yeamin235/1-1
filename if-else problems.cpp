548bbf8b9b424cd59223c59af9bfaed9aaa7dcfe
Yeamin235 <38450577+Yeamin235@users.noreply.github.com>
2018-08-07 20:29:33 +0600

    Add files via upload

---
+++ if-else problems.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+int num;
+
+cout<<"Enter the number:"<<endl;
+
+cin>>num;
+
+
+if(num%2==0)
+
+{
+    cout<<"The number "<<num<< " is Even" <<endl;
+}
+
+else {
+
+    cout<<"The number "<<num<<" is Odd"<<endl;
+}
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
