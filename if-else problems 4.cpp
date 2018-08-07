1ded45c184435163ed475edd2d8e86ed71932ed4
Yeamin235 <38450577+Yeamin235@users.noreply.github.com>
2018-08-07 20:47:20 +0600

    Add files via upload

---
+++ if-else problems 4.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+int num;
+
+cout<<"Enter the num:"<<endl;
+
+cin>>num;
+
+
+
+if(num<0)
+
+{
+    cout<<"This number "<<-num<< " has been made an absolute value .." <<endl;
+}
+
+else {
+
+    cout<<"This number "<<num<<" is an absolute value ......"<<endl;
+}
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
