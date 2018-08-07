e3057211b98b2347cd993fcbd48f9b9dee5c8b70
Yeamin235 <38450577+Yeamin235@users.noreply.github.com>
2018-08-07 20:41:33 +0600

    Add files via upload

---
+++ if-else problems 3.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+int marks;
+
+cout<<"Enter the marks:"<<endl;
+
+cin>>marks;
+
+
+
+if(marks>=33)
+
+{
+    cout<<"This marks "<<marks<< " is a pass mark.." <<endl;
+}
+
+else {
+
+    cout<<"This marks "<<marks<<" is a fail mark......."<<endl;
+}
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
