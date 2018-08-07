17f2d709b6621941e0277dc3f723ab10c1ca490c
Yeamin235 <38450577+Yeamin235@users.noreply.github.com>
2018-08-07 20:37:38 +0600

    Add files via upload

---
+++ if-else problems 2.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+int num1,num2;
+
+cout<<"Enter the number:"<<endl;
+
+cin>>num1;
+
+cout<<"Enter another number:"<<endl;
+
+cin>>num2;
+
+if(num1>num2)
+
+{
+    cout<<"The number "<<num1<< " is largest" <<endl;
+}
+
+else {
+
+    cout<<"The number "<<num2<<" is largest"<<endl;
+}
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
