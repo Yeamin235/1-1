8cb17ada19e4baedae2bc26e1f4c43603bd758ad
Yeamin235 <38450577+Yeamin235@users.noreply.github.com>
2018-09-09 02:06:32 +0600

    Add files via upload

---
+++ encapsulation.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+class student{
+
+    private:
+
+     string name;
+
+    public:
+
+
+void setname(string x){
+
+name=x;
+
+}
+
+string getname(){
+
+
+return name;
+
+}
+
+
+} ;
+
+int main(){
+
+student s1;
+
+s1.setname("i am coding!!!");
+
+cout<<s1.getname()<<endl;
+
+
+
+
+
+
+
+return 0;
+}
