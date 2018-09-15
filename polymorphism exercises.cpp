f2aa32b6d9e65823c3d513d9b54ce961f9d3e400
Yeamin235 <38450577+Yeamin235@users.noreply.github.com>
2018-09-15 17:17:13 +0600

    Add files via upload

---
+++ polymorphism exercises.cpp
@@ -0,0 +1,94 @@
+#include<iostream>
+
+using namespace std;
+
+class shape{
+
+    public:
+
+    double dim1,dim2;
+
+    shape(double dim1,double dim2)
+    {
+    this -> dim1=dim1;
+
+    this -> dim2=dim2;
+
+
+    }
+
+virtual double area()
+{
+
+    return 0;
+}
+
+} ;
+
+class triangle: public shape
+{
+
+public:
+
+    triangle(double dim1,double dim2)
+
+    :shape(dim1,dim2)
+    {
+
+
+    }
+
+    double area()
+    {
+
+    return 0.5*dim1*dim2;
+
+    }
+
+};
+
+
+class rectangle: public shape
+{
+
+public:
+
+    rectangle(double dim1,double dim2)
+
+    :shape(dim1,dim2)
+    {
+
+
+    }
+
+    double area()
+    {
+
+    return dim1*dim2;
+
+    }
+
+};
+
+
+int main()
+{
+
+
+    shape *s;
+
+    triangle t(10,20);
+
+    rectangle r(10,30);
+
+    s=&t;
+
+    cout<<"Area of triangle :"<<s -> area() <<endl;
+
+    s=&r;
+        cout<<"Area of rectangle :"<<s -> area() <<endl;
+
+
+
+    return 0;
+}
